ba3b7138bff7f65cfc16c9e2099a46c9ed7a58a3
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-08-06 22:46:03 +0100

    set up select genre and era functionality

--- src/components/SelectGenre.vue
+++ src/components/SelectGenre.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h5>Please select a genre</h5>
-    <v-btn @click="getTracks">Indie</v-btn>
+    <v-btn v-for="(item, index) in availableGenres" :key="index" @click="$event => setGenre(index)">{{index}}</v-btn>
   </div>
 
 </template>
@@ -10,9 +10,17 @@
 import spotifyUtils from '../utils/spotify'
 export default {
   name: 'SelectGenre',
+  computed: {
+    availableGenres() {
+      return this.$store.state.genres
+    }
+  },
   methods: {
     async getTracks() {
       spotifyUtils.getPlaylistTracks('37i9dQZF1DX7hXnntRGfka')
+    },
+    setGenre(value) {
+      this.$store.commit('setState', {property: 'selectedGenre', value})
     }
   }
 }
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,16 +2,24 @@
   <div>
     <h1>Welcome to song quiz</h1>
     <SelectGenre />
+    <SelectEra v-if="selectedGenre"/>
   </div>
 </template>
 
 <script>
 import spotifyUtils from '../utils/spotify'
 import SelectGenre from '../components/SelectGenre.vue'
+import SelectEra from '../components/SelectEra.vue'
 export default {
   name: "Landing",
   components: {
-    SelectGenre
+    SelectGenre,
+    SelectEra
+  },
+  computed: {
+    selectedGenre() {
+      return this.$store.state.selectedGenre
+    }
   },
   async mounted() {
     const accessToken = await spotifyUtils.getAccessToken()
