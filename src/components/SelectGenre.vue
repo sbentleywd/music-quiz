c8c2fecd2c37fb1b02dbc05f19e39252bc5bd566
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2021-08-06 21:58:03 +0100

    Add select genre component

---
+++ src/components/SelectGenre.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h5>Please select a genre</h5>
+    <v-btn @click="getTracks">Indie</v-btn>
+  </div>
+
+</template>
+
+<script>
+import spotifyUtils from '../utils/spotify'
+export default {
+  name: 'SelectGenre',
+  methods: {
+    async getTracks() {
+      spotifyUtils.getPlaylistTracks('37i9dQZF1DX7hXnntRGfka')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Welcome to song quiz</h1>
+  <div>
+    <h1>Welcome to song quiz</h1>
+    <SelectGenre />
+  </div>
 </template>
 
 <script>
-import spotifyUtils from "../utils/spotify";
+import spotifyUtils from '../utils/spotify'
+import SelectGenre from '../components/SelectGenre.vue'
 export default {
   name: "Landing",
+  components: {
+    SelectGenre
+  },
   async mounted() {
     const accessToken = await spotifyUtils.getAccessToken()
     this.$store.commit('setState', { property: 'accessToken', value: accessToken})
